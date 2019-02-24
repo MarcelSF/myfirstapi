601c6f5bfa66c5e0bd47779d92d3b77d25cd537b
marcel fonseca <marcelsf23br@gmail.com>
2019-02-24 14:04:27 -0300

    perfected the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destroying DB"
+
+Item.destroy_all
+Todo.destroy_all
 
 puts "creating Todos"
 #creating Todos
